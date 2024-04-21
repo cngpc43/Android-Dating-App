c8c9b6530c3bd7a9ad2ceb85331bb95a40fe76c1
duc3503 <duc.huynhhuu3523@gmail.com>
2024-04-21 17:30:22 +0700

    add back button

--- app/src/main/java/com/example/mymessengerapp/email_change.java
+++ app/src/main/java/com/example/mymessengerapp/email_change.java
@@ -1,18 +1,30 @@
 package com.example.mymessengerapp;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class email_change extends AppCompatActivity {
+    ImageView back_icon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_email_change);
         if (getSupportActionBar() != null) {
-
             getSupportActionBar().hide();
         }
 
+        back_icon = findViewById(R.id.back_icon);
+        back_icon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(email_change.this, account_settings.class);
+                startActivity(intent);
+            }
+        });
     }
 }
