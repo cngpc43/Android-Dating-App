4608e803c5fc2dafd047de804a832697435f2c56
Đức Huỳnh Hữu <duc.huynhhuu3523@gmail.com>
2024-07-01 03:46:55 +0700

    feat: ask for notification permission

--- app/src/main/java/com/example/mymessengerapp/MainActivity.java
+++ app/src/main/java/com/example/mymessengerapp/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.mymessengerapp;
 
 import androidx.activity.OnBackPressedCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.Manifest;
 
 
 import android.content.Intent;
@@ -22,6 +27,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textview.MaterialTextView;
@@ -47,6 +53,16 @@ public class MainActivity extends AppCompatActivity {
     MaterialTextView title;
     Boolean doubleBackToExitPressedOnce = false;
     ValueEventListener onlineValueListener;
+    // Declare the launcher at the top of your Activity/Fragment:
+    private final ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    // FCM SDK (and your app) can post notifications.
+                } else {
+                    // TODO: Inform user that that your app will not show notifications.
+                    Toast.makeText(this, "You will not receive notifications", Toast.LENGTH_SHORT).show();
+                }
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -132,6 +148,9 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
 
+            // request notification permission
+            askNotificationPermission();
+
         } else {
             // No authenticated user handler -> LOGIN
             Toast.makeText(this, "No authenticated user", Toast.LENGTH_SHORT).show();
@@ -151,6 +170,8 @@ public class MainActivity extends AppCompatActivity {
                 selectChat();
             else if (getIntent().getStringExtra("fragment").equals("notification"))
                 selectNotification();
+            else if (getIntent().getStringExtra("fragment").equals("matching_requests"))
+                selectMatchingRequest();
             else
                 selectHome();
         }
@@ -206,6 +227,26 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+
+
+    private void askNotificationPermission() {
+        // This is only necessary for API level >= 33 (TIRAMISU)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) ==
+                    PackageManager.PERMISSION_GRANTED) {
+                // FCM SDK (and your app) can post notifications.
+            } else if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
+                // TODO: display an educational UI explaining to the user the features that will be enabled
+                //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
+                //       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
+                //       If the user selects "No thanks," allow the user to continue without notifications.
+            } else {
+                // Directly ask for the permission
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS);
+            }
+        }
     }
 
     private boolean loadFragment(Fragment fragment, String title) {
@@ -282,11 +323,27 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void selectMatchingRequest() {
+        if (loadFragment(new MatchingRequestsFragment(), "Notifications")) {
+            home_selected.setBackground(null);
+            chat_selected.setBackground(null);
+            user_selected.setBackground(null);
+            noti_selected.setBackground(ContextCompat.getDrawable(this, R.drawable.selected_nav_item));
+
+            ic_home.setColorFilter(Color.BLACK);
+            ic_chat.setColorFilter(Color.BLACK);
+            ic_user.setColorFilter(Color.BLACK);
+            ic_noti.setColorFilter(Color.rgb(236, 83, 131));
+        }
+    }
+
     @Override
     public void onDestroy() {
         super.onDestroy();
         if (onlineRef != null && onlineValueListener != null)
             onlineRef.removeEventListener(onlineValueListener);
     }
+
+
 
 }
